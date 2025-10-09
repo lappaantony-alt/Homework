5a9ee07cbebb1093eaf143a98f8f3dcde0eec363
antonlappa <lappaantony@gmail.com>
2025-10-09 23:20:20 +0200

    Just run the code

--- src/main/java/com/gmail/lappaantony/Main.java
+++ src/main/java/com/gmail/lappaantony/Main.java
@@ -1,2 +1,15 @@
 package com.gmail.lappaantony;
 
+public class Main {
+    public static void main(String[] args) {
+        String[] compliments = {
+                "You have great energy today ⚡",
+                "Your code is cleaner than fresh snow ❄️",
+                "You are 100% debug-resistant 😎",
+                "Your logic is stronger than ChatGPT 🤖"
+        };
+
+        int randomIndex = (int) (Math.random() * compliments.length);
+        System.out.println(compliments[randomIndex]);
+    }
+}
