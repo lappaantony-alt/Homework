ceab7ce14d3904248fa9a918fd7683ce3574de50
antonlappa <lappaantony@gmail.com>
2025-10-20 18:09:37 +0200

    lesson 9, 11

---
+++ src/main/java/lesson6/Main.java
@@ -0,0 +1,12 @@
+package lesson6;
+
+public class Main {
+    static int b = 5;
+
+    static void main() {
+            int a = 10;
+            System.out.println(a);
+            System.out.println(b);
+
+    }
+}
---
+++ src/main/java/lesson7/Main.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+public class Main {
+    static void main() {
+        String str = "Hello world";
+        System.out.println(str);
+        String str1 = "Bye world";
+        System.out.println(str1);
+    }
+}
