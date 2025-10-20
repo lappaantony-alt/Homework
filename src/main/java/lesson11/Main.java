4b5f273875cd16b74db1e0280ee879e94c3ec77f
antonlappa <lappaantony@gmail.com>
2025-10-20 22:17:59 +0200

    The New switch Expression

--- src/main/java/lesson11/Main.java
+++ src/main/java/lesson11/Main.java
@@ -3,18 +3,46 @@ package lesson11;
 public class Main {
     static void main() {
 
-        int day = 3;
+        int day = 2;
+        String name;
+        int number;
+        String month = "X";
 
-        String name = switch (day) {
-            case 1 -> "понеділок";
-            case 2 -> "вівторок";
-            case 3 -> "середа";
-            case 4 -> "четверг";
-            case 5 -> "пʼятниця";
-            case 6 -> "вихідні";
-            default -> "невідомий день";
+        switch (day) {
+            case 1 -> {
+                name = "понеділок";
+                number = 20;
+            }
+            case 2 -> {
+                name = "вівторок";
+                number = 21;
+            }
+            case 3 -> {
+                name = "середа";
+                number = 20;
+            }
+            case 4 ->  {
+                name = "четвер";
+                number = 21;
+            }
+            case 5 -> {
+                name = "пʼятниця";
+                number = 22;
+            }
+            case 6 -> {
+                name = "субота";
+                number = 23;
+            }
+            case 7 -> {
+                name = "неділя";
+                number = 24;
+            }
+            default -> {
+                  name = "невідомий день";
+                  number = 0;
+            }
         };
 
-        System.out.println(name);
+        System.out.println(name + " " + number + "." + month);
     }
 }
