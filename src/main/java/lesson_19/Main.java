f5a7774205aea877e2dfb59c893450830e7333cf
antonlappa <lappaantony@gmail.com>
2025-10-31 12:53:01 +0100

    update

---
+++ src/main/java/lesson_19/Main.java
@@ -0,0 +1,38 @@
+package lesson_19;
+
+public class Main {
+    static void main() {
+        // Використовуємо стандартний джава клас Math
+        System.out.println("Використовуємо стандартний джава клас Math");
+        // знаходження максимального числа
+        int max = Math.max(0, 1);
+
+        // Округлення в більший бік
+        double ceiled = Math.ceil(4.3);
+        System.out.println(ceiled);
+
+        // Округлення в менший бік
+        double floored = Math.floor(4.3);
+        System.out.println(floored);
+
+        // Округлення до найближчого цілого
+        double rounded1 = Math.round(4.3);
+        System.out.println(rounded1);
+        double rounded2 = Math.round(4.5);
+        System.out.println(rounded2);
+
+        // Використовуємо самописний клас MyMath
+        System.out.println("Використовуємо самописний клас MyMath");
+        int min = MyMath.min(100, 20);
+        System.out.println(min);
+
+        double absed = MyMath.abs(-4.3);
+        System.out.println(absed);
+
+        int powered = MyMath.power(2, 3);
+        System.out.println(powered);
+
+        int rounded = MyMath.round(4.1);
+        System.out.println(rounded);
+    }
+}
