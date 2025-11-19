2ca088b4c899f64551756c725e0ce5a201aa9ca2
antonlappa <lappaantony@gmail.com>
2025-11-19 01:13:16 +0100

    Creating stack

---
+++ src/main/java/lesson_30/StackImpl.java
@@ -0,0 +1,80 @@
+package lesson_30;
+
+public class StackImpl implements Stackable {
+
+    private int maxSize;
+    private int[] stackArr;
+    private int top;
+
+    public StackImpl(int max) {
+        this.maxSize = max;
+        stackArr = new int[maxSize];
+        top = -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        if (top == maxSize - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void addElement(int element) {
+        if (isFull()) {
+            System.out.println("Easy, cowboy! This stack is already full🤠");
+            return;
+        } else {
+            stackArr[++top] = element;
+        }
+    }
+
+    @Override
+    public Integer takeOutElement() {
+        if (isEmpty()) {
+            System.out.println("Did you put anything there to take it out?🤨");
+            return null;
+        }
+        int element = stackArr[top];
+        top--;
+        return element;
+    }
+
+    @Override
+    public Integer readTop() {
+        if (isEmpty()) {
+            System.out.println("For check top, you have to put there something😉");
+            return null;
+        }
+        return stackArr[top];
+    }
+
+    @Override
+    public void printStack() {
+        if (isEmpty()) {
+            System.out.println("I guess your forgot to fill in stack😅");
+            return;
+        }
+        System.out.println("Stack content (don't forget about LIFO🤓)");
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stackArr[i]);
+        }
+    }
+
+    @Override
+    public void clear() {
+        if (isEmpty()) {
+            System.out.println("There is nothing to clean🫠");
+        }
+        top = -1;
+    }
+}
