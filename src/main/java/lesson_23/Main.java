5bf4b9ec77eb0d2196d1d359cbe366cb106b43f7
antonlappa <lappaantony@gmail.com>
2025-12-01 23:37:33 +0100

    bug fix

--- src/main/java/lesson_23/Main.java
+++ src/main/java/lesson_23/Main.java
@@ -1,41 +1,52 @@
+package lesson_23;
+
 public class Main {
-    public static int lengthOfLastWord(String s) {
+
+
+    public static int lengthOfLastWordRegex(String s) {
         s = s.trim();
         return s.replaceAll(".*\\b(\\w+)\\W*$", "$1").length();
     }
 
-public static void main(String[] args) {
-    System.out.println(lengthOfLastWord("Hello World"));
-    System.out.println(lengthOfLastWord(" fly me to the moon "));
-    System.out.println(lengthOfLastWord("Luffy is still joyboy"));
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWordRegex("Hello World"));
+        System.out.println(lengthOfLastWordRegex(" fly me to the moon "));
+        System.out.println(lengthOfLastWordRegex("Luffy is still joyboy"));
+
+    }
+
+    // Варіант через
+    //lastiIndexOf()
+
+
+    public static int lengthOfLastWordIndex(String s) {
+        s = s.trim();
+        int lastSpaceIndex = s.lastIndexOf(' ');
+
+        if (lastSpaceIndex == -1) {
+            return s.length();
+        }
+
+        return s.length() - lastSpaceIndex - 1;
+    }
+
+
+//    Варіант 3
+//    цикл з
+//    кінця рядка
+//    той який
+//    краще запамʼятати.
+
+
+    public static int lengthOfLastWordWordLoop(String s) {
+        s = s.trim();
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') break;
+            count++;
+        }
+        return count;
+    }
+
 }
-}
-
-     // Варіант через lastiIndexOf()
-//public class Main {
-//    public static int lengthOfLastWord(String s) {
-//        s = s.trim();
-//        int lastSpaceIndex = s.lastIndexOf(' ');
-//
-//        if (lastSpaceIndex == -1) {
-//            return s.length();
-//        }
-//
-//        return s.length() - lastSpaceIndex - 1;
-//    }
-//}
-
-
-   // Варіант 3 цикл з кінця рядка  той який краще запамʼятати.
-//public class Main {
-//    public static int lengthOfLastWord(String s) {
-//        s = s.trim();
-//        int count = 0;
-//
-//        for (int i = s.length() - 1; i >= 0; i--) {
-//            if (s.charAt(i) == ' ') break;
-//            count++;
-//        }
-//        return count;
-//    }
-//}
